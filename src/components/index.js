099681215cc1ba15be0127fd2b9a39602a0a02b9
insulineru <insuline.ru@gmail.com>
2020-07-29 00:15:05 +0300

    feat(app): перенес вёрстку в отдельные компоненты

--- src/components/index.js
+++ src/components/index.js
@@ -1,1 +1,4 @@
+export { default as Categories } from './Categories';
 export { default as Header } from './Header';
+export { default as PizzaBlock } from './PizzaBlock';
+export { default as SortSelect } from './SortSelect';
