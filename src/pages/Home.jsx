530005137d4546890882937a4c2658d4e70b82df
insulineru <insuline.ru@gmail.com>
2020-08-01 01:10:32 +0300

    feat(app): добавил запрос с сервера и обработал полученные данные

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
+import axios from 'axios';
 
-import { PizzaBlock, Categories, SortSelect } from '../components';
+import { PizzaBlock, Categories, SortSelect, LoadingPizzaBlock } from '../components';
 
 const categories = ['Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
 const sortByItems = [
@@ -13,6 +14,19 @@ function Home() {
   const [category, setCategory] = React.useState(null);
   const [sortBy, setSortBy] = React.useState(sortByItems[0]);
 
+  const [pizzas, setPizzas] = React.useState([]);
+  const [loading, setLoading] = React.useState(false)
+
+  React.useEffect(() => {
+    setLoading(true)
+    axios
+      .get('http://localhost:1337/pizzas', {})
+      .then(({ data }) => {
+        setPizzas(data);
+        setLoading(false);
+      });
+  }, [sortBy, category]);
+
   return (
     <div className="content">
       <div className="container">
@@ -22,9 +36,12 @@ function Home() {
         </div>
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
-          {
+          {loading ?
             [...Array(12)].map( (item, index) => {
-              return (<PizzaBlock key={ index }/>)
+              return (<LoadingPizzaBlock key={ index }/>)
+            }) :
+            pizzas.map(item => {
+              return (<PizzaBlock item={item} key={item.id}/>)
             })
           }
         </div>
